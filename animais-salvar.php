436b8af330ad564d906aa4245ef53bb335b01bad
ana.mf <froggitfroggers@gmail.com>
2026-05-26 22:14:55 -0300

    Update animais-salvar.php

--- animais-salvar.php
+++ animais-salvar.php
@@ -8,7 +8,7 @@ $url_imagem = $_POST['idade'];
 
 include "inc-conexao.php";
 
-$resultado = mysqli_execute_query($conn, "INSERT INTO tb_informacoes_gatos(nome, vermifugado, possui-doenca, castrado, idade, foto) VALUES
+$resultado = mysqli_execute_query($conn, "INSERT INTO tb_informacoes_gatos(nome, vermifugado, possui_doenca, castrado, idade, foto) VALUES
 (?, ?, ?, ?, ?, ?)", [$nome, $foi_vermifugado, $possui_doenca, $foi_castrado, $url_imagem]);
 
 mysqli_close($conn);
