9c84a4ae7bb368ae1879a05f0751b352520923c3
Nan Santos <pixelado405@gmail.com>
2026-05-26 22:17:14 -0300

    Correções ao salvar as informações do formulário no banco de dados!

--- animais-salvar.php
+++ animais-salvar.php
@@ -4,12 +4,13 @@ $nome = $_POST['nome'];
 $foi_vermifugado = $_POST['foi_vermifugado'];
 $possui_doenca = $_POST['possui_doenca'];
 $foi_castrado = $_POST['foi_castrado'];
-$url_imagem = $_POST['idade'];
+$idade = $_POST['idade'];
+$url_imagem = $_POST['url_imagem'];
 
 include "inc-conexao.php";
 
 $resultado = mysqli_execute_query($conn, "INSERT INTO tb_informacoes_gatos(nome, vermifugado, possui_doenca, castrado, idade, foto) VALUES
-(?, ?, ?, ?, ?, ?)", [$nome, $foi_vermifugado, $possui_doenca, $foi_castrado, $url_imagem]);
+(?, ?, ?, ?, ?, ?)", [$nome, $foi_vermifugado, $possui_doenca, $foi_castrado, $idade, $url_imagem]);
 
 mysqli_close($conn);
 
