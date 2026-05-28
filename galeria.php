3bb6b2ea2dc929ff2ff51d51e9f7775e322cd295
Iori <iorilima13@gmail.com>
2026-05-28 20:14:28 -0300

    Update galeria.php

--- galeria.php
+++ galeria.php
@@ -13,182 +13,60 @@ include "inc-head.php";
 
     <!-- cards linha1 -->
     <main class="container my-4">
+         <div class="row row-cols-4 g-4">
         <!-- card 1 -->
-        <div class="row row-cols-4 g-4">
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/Romeuu.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Romeu</h5>
-                                <p class="card-text">Gatão branco de olhos azuis </p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Raça: Gatoxinha Função: Gordo</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <?php 
+        $nomeGato = "Romeu"; $infoFrent = "Gatão branco de olhos azuis"; $infoTras = "Raça: Gatoxinha Função: Gordo"; $fotoCat = "Romeuu.jpg";
+        include "inc-card.php";
+        ?> 
             <!-- card 2 -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/NeneRycharlison.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                 <h5 class="card-title">Rycharlison</h5>
-                                <p class="card-text">Coisas serao colocadas aqui</p>
-                            </div>
-                        </div>
+            <?php
+             $nomeGato = "Rycharlison"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "NeneRycharlison.jpg";
+             include "inc-card.php";
+            ?>
 
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- card 3 -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/irma-xena.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                 <h5 class="card-title">Irma Xena</h5>
-                                <p class="card-text">Coisas serao colocadas aqui</p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+             <?php
+             $nomeGato = "Irma Xena"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "irma-xena.jpg";
+             include "inc-card.php";
+            ?>
+            
             <!-- card 4 -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/Rycharlison.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Mine-Rycharlison</h5>
-                                <p class="card-text">Coisas serao colocadas aqui </p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php
+             $nomeGato = "Mine-Rycharlison"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "Rycharlison.jpg";
+             include "inc-card.php";
+            ?>
         </div>
     </main>
     <!-- cards linha2 -->
     <main class="container my-4">
-        <!-- card 1 -->
         <div class="row row-cols-4 g-4">
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/cat-peso.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Bitelinho</h5>
-                                <p class="card-text">Coisas serao colocadas aqui </p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <!-- card 1 --> 
+            <?php
+             $nomeGato = "Bitelinho"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "cat-peso.jpg";
+             include "inc-card.php";
+            ?>
+            
             <!-- card 2 -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/cat-schrodinger.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Gato de Schrodinger</h5>
-                                <p class="card-text">Coisas serao colocadas aqui </p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+             <?php
+             $nomeGato = "Gato de Schrodinger"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "cat-schrodinger.jpg";
+             include "inc-card.php";
+            ?>
             <!-- card 3  -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/cat-invert.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Spider-Cat</h5>
-                                <p class="card-text">Coisas serao colocadas aqui </p>
-                            </div>
-                        </div>
+             <?php
+             $nomeGato = "Spider-Cat"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "cat-invert.jpg";
+             include "inc-card.php";
+            ?>
 
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- card 4 -->
-            <div class="col">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="card h-100 flip-card-front">
-                            <img src="./img/gatoguerra.jpg" class="card-img-top object-fit-cover" alt="..." height="300px">
-                            <div class="card-body">
-                                <h5 class="card-title">Soldado Hyan</h5>
-                                <p class="card-text">Coisas serao colocadas aqui </p>
-                            </div>
-                        </div>
-
-                        <div class="card h-100 flip-card-back">
-                            <div class="card-body">
-                                <h5 class="card-title">Mais informações</h5>
-                                <p class="card-text">Coisas mais importantes e detalhadas virão aqui</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+             <?php
+             $nomeGato = "Soldado Hyan"; $infoFrent = "..."; $infoTras = "..."; $fotoCat = "gatoguerra.jpg";
+             include "inc-card.php";
+            ?>
         </div>
     </main>
+<!-- cards linha3 -->
+
 
     <script>
         document.querySelectorAll('.flip-card').forEach((card) => {
