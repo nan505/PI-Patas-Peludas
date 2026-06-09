6c7f0922b3db134c0401f5eed1bcab966acbcb8f
Iori <iorilima13@gmail.com>
2026-06-08 22:17:15 -0300

    Update adotantes-formulario.php

--- adotantes-formulario.php
+++ adotantes-formulario.php
@@ -74,4 +74,8 @@ include "inc-head.php";
 
 </section>
 
+<<<<<<< HEAD
 <?php include "inc-footer.php"; ?>
+=======
+<?php include "inc-footer-admin.php"; ?>
+>>>>>>> 443c6f6795356a1fa20011c2f884a00800188fb7
