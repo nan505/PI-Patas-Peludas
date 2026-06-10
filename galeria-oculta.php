35ad2906c412464eeec64f2c65c351003083ee68
Iori <iorilima13@gmail.com>
2026-06-10 20:14:30 -0300

    Update galeria-oculta.php

--- galeria-oculta.php
+++ galeria-oculta.php
@@ -21,7 +21,7 @@ include "inc-head.php";
   </div>
 </div>
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 18rem ;">
   <img class="card-img-top" src="./img/na.jpg" alt="Card image cap">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
