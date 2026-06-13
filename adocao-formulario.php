249d782ce74ddf385ef88d393d5585c9126df55f
ana.mf <froggitfroggers@gmail.com>
2026-06-13 11:42:18 -0300

    adicao do pdf

---
+++ adocao-formulario.php
@@ -0,0 +1,150 @@
+<body>
+    <h1 class="titulo-ong">Patinhas Peludas</h1>
+
+    <form action="gerar-pdf.php" method="post">
+    <section>
+    <h2>Dados do Adotante:</h2>
+    <label>Nome do Adotante:</label>
+    <input type="text" name="nome">
+
+    <br><br>
+
+    <label>Endereço:</label>
+    <input type="text" name="endereco">
+
+    <br><br>
+
+    <label>RG:</label>
+    <input type="text" name="rg">
+
+    <br><br>
+
+    <label>CPF/CNPJ:</label>
+    <input type="text" name="cpf">
+
+    <br><br>
+
+    <label>Telefone para contato:</label>
+    <input type="tel" name="telefone">
+
+    <br><br>
+
+    <label>E-mail:</label>
+    <input type="email" name="email">
+    </section>
+    <br><br>
+<section>
+    <h2>DADOS DO ANIMAL ADOTADO</h2>
+
+<div>
+    <label>Nome do animal:</label>
+    <input type="text" name="nomeAnimal">
+</div>
+
+<br>
+
+<div>
+    <label>Raça:</label>
+    <input type="text" name="raca">
+
+    <br><br>
+
+    <label>Idade:</label>
+    <input type="number" name="idade">
+</div>
+
+<br>
+
+<div>
+    <label>Porte:</label>
+
+    <input type="radio" name="porte" value="Mini">
+    <label>Mini</label>
+
+    <input type="radio" name="porte" value="Pequeno">
+    <label>Pequeno</label>
+
+    <input type="radio" name="porte" value="Médio">
+    <label>Médio</label>
+
+    <input type="radio" name="porte" value="Grande">
+    <label>Grande</label>
+</div>
+
+<br>
+
+<div>
+    <label>Espécie:</label>
+
+    <input type="radio" name="especie" value="Canina">
+    <label>Canina</label>
+
+    <input type="radio" name="especie" value="Felina">
+    <label>Felina</label>
+</div>
+
+<br>
+
+<div>
+    <label>Sexo:</label>
+
+    <input type="radio" name="sexo" value="Fêmea">
+    <label>Fêmea</label>
+
+    <input type="radio" name="sexo" value="Macho">
+    <label>Macho</label>
+</div>
+
+<br>
+
+<div>
+    <label>Castrado?</label>
+
+    <input type="radio" name="castrado" value="Sim">
+    <label>Sim</label>
+
+    <input type="radio" name="castrado" value="Não">
+    <label>Não</label>
+</div>
+
+<br>
+
+<div>
+    <label>Vermifugado nos últimos três meses?</label>
+
+    <input type="radio" name="vermifugado" value="Sim">
+    <label>Sim</label>
+
+    <input type="radio" name="vermifugado" value="Não">
+    <label>Não</label>
+
+    <input type="radio" name="vermifugado" value="Sem informação">
+    <label>Sem informação</label>
+</div>
+
+<br>
+
+<div>
+    <label>Vacinado?</label>
+
+    <input type="radio" name="vacinado" value="Sim">
+    <label>Sim</label>
+
+    <input type="radio" name="vacinado" value="Não">
+    <label>Não</label>
+
+    <input type="radio" name="vacinado" value="Sem informação">
+    <label>Sem informação</label>
+</div>
+
+<br>
+
+<div>
+    <label>Observações:</label>
+    <input type="text" name="observacoes">
+</div>
+</section>
+<button type="submit" name="btn-gerar" value="Gerar PDF">Gerar PDF</button>
+
+</form>
+</body>
---
+++ gerar-pdf.php
@@ -0,0 +1,119 @@
+<?php
+require 'vendor/autoload.php';
+
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+$conteudo = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+if (!$conteudo) {
+    header("Location: adocao-formulario.php");
+    exit;
+}
+
+ob_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario de Adotante</title>
+</head>
+<body>
+
+<h1>Patinhas Peludas</h1>
+<section>   
+    <h2>Informações do Adotante:</h2>
+    
+    <p>Nome: <?= $conteudo['nome'] ?></p>
+    <p>Endereço: <?= $conteudo['endereco'] ?></p>
+    <p>RG: <?= $conteudo['rg'] ?></p>
+    <p>CPF: <?= $conteudo['cpf'] ?></p>
+    <p>Telefone: <?= $conteudo['telefone'] ?></p>
+    <p>Email: <?= $conteudo['email'] ?></p>
+</section>
+    
+    <section>
+    <h2>Informações do Animal</h2>
+
+    <p>Nome: <?= $conteudo['nomeAnimal'] ?></p>
+    <p>Raça: <?= $conteudo['raca'] ?></p>
+    <p>Idade: <?= $conteudo['idade'] ?></p>
+    <p>Porte: <?= $conteudo['porte'] ?></p>
+    <p>Espécie: <?= $conteudo['especie'] ?></p>
+    <p>Sexo: <?= $conteudo['sexo'] ?></p>
+    <p>Castrado: <?= $conteudo['castrado'] ?></p>
+    <p>Vermifugado nos últimos três meses? <?= $conteudo['vermifugado'] ?></p>
+    <p>Vacinado: <?= $conteudo['vacinado'] ?></p>
+    <p>Observações: <?= $conteudo['observacoes'] ?></p>
+</section>
+    
+<section>
+        <h2>DECLARAÇÃO DO(A) ADOTANTE:</h2>
+        <p>
+        Eu (nome completo) ______________________________________________________, ciente das minhas obrigações enquanto adotante do(a) ____________________________________________ (nome do animal), declaro-me apto(a) para assumir a guarda e a responsabilidade sobre o mesmo, eximindo o doador de toda e qualquer responsabilidade por quaisquer atos praticados pelo e com o animal a partir desta data, exceto no que diz respeito à castração, se assim foi acordado.
+    </p>
+<br>
+<h3>Declaro ainda que:</h3>
+    <p>
+     1. Comprometo-me a realizar visitas periódicas ao veterinário, bem como a manter a vacinação e vermifugação em dia, arcando com todas as despesas e dedicando todo o tempo necessário para que o animal adotado tenha plena saúde e qualidade de vida, estando ciente de que cães e gatos podem chegar a viver até 20 anos. 
+    </p>
+    
+    <p>
+        2. Atesto que já tomei (ou tomarei muito em breve) todas as medidas possíveis para garantir a segurança e o bem-estar do animal adotado, como por exemplo telas em janelas de apartamentos, cercas ou muros em casas com pátio, coleira e guia para passeios diários, local para água e alimento e um local confortável para o descanso. 
+    </p>
+
+    <p>
+        3. Se minha residência for um apartamento, comprometo-me a realizar no mínimo dois passeios diários com o animal para que ele possa fazer suas necessidades e realizar atividades para gasto de energia.
+    </p>
+
+    <p>
+        4. Se minha residência for uma casa, comprometo-me a deixar o animal sempre solto no pátio e com acesso livre a espaços cobertos da casa, de forma que sempre fique protegido das intempéries (frio, calor, chuva etc). 
+    </p>
+
+    <p>
+        5. Comprometo-me a nunca, sob qualquer hipótese, manter o animal adotado preso à qualquer tipo de corrente ou em cubículos ou espaços minúsculos.
+    </p>
+
+    <p>
+        6. Caso eu precise mudar de residência, comprometo-me a sempre pensar no animal adotado para fazer minhas escolhas, assegurando que ele possa me acompanhar e estar sempre protegido por mim até o final de sua vida.
+    </p>
+
+    <p>
+        7. Asseguro que tenho com quem deixar o animal em caso de viagens ou afastamentos temporários.
+    </p>
+
+    <p>
+        8. Comprometo-me a nunca deixar o animal sozinho por mais de 10h, providenciando sempre pessoas que possam cuidá-lo e alimentá-lo caso necessite de períodos maiores de afastamento.
+    </p>
+
+    <p>
+        9. Comprometo-me, também, a permitir o acesso do doador, e a quem com ele se encontrar, ao local de residência do animal, para averiguação de suas condições. 
+    </p>
+    <p>
+             Tenho conhecimento de que, caso seja constatado por parte do doador ou qualquer pessoa ou órgão, que o animal se encontra em situação inadequada para seu bem-estar, perderei a sua guarda, sem prejuízo das penalidades legais, administrativas, cíveis e criminais. Declaro-me, assim, ciente das normas acima, as quais aceito, assinando o presente Termo de Adoção Responsável, assumindo plenamente os deveres que dele constam, bem como outros relacionados à posse responsável e que não estejam incluídos neste Termo.  
+    </p>
+    <p>__________________________________ , ______ de _______________________ de 20______</p>
+
+    <div class="assinaturas">
+        <p>Assinatura do Doador</p>
+        <p>CPF / CNPJ: ________________________</p>
+        <p>Assinatura do Adotante</p>
+        <p>CPF / CNPJ: ________________________</p>
+    </div>
+
+    </section>
+</body>
+</html>
+
+<?php
+$html = ob_get_clean();
+
+$dompdf->loadHtml($html);
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+$dompdf->stream("formulario.pdf", ["Attachment" => false]);
+
+?>
