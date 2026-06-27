4dd491ab9eda2e052fadaa9138b044ff74e30e70
Iori <iorilima13@gmail.com>
2026-06-26 21:23:33 -0300

    style

--- index.php
+++ index.php
@@ -5,62 +5,66 @@ include "inc-head.php";
 
 ?>
 
-<body id="body-index" class="pt-5 d-flex flex-column min-vh-100" data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="0" tabindex="0" style="scroll-behavior: smooth" >
+<body id="body-index" class="pt-5 d-flex flex-column min-vh-100" data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="0" tabindex="0" style="scroll-behavior: smooth">
     <header>
         <?php include "inc-nav.php"; ?>
 
-
-<!-- Carrossel feito por: Nan Santos -->
-<!-- Importante: Mudar o "src" e o "alt" das imagens assim que possível -->
-        <div id="indicadoresCarrossel" class="carousel slide" data-bs-ride="carousel">
+        <div id="indicadoresCarrossel" class="carousel slide home-hero" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#indicadoresCarrossel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#indicadoresCarrossel" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#indicadoresCarrossel" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
+
             <div class="carousel-inner ratio ratio-21x9 banner-carrossel">
                 <div class="carousel-item active">
-                    <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" 
-                    class="d-block w-100 img-fluid h-100 object-fit-cover" alt="DESCRIÇÃO TEMPORÁRIA, ALTERAR DEPOIS">
+                    <img src="https://inovaveterinaria.com.br/wp-content/uploads/2015/04/gato-sem-raca-INOVA-scaled.jpg" class="d-block w-100 img-fluid h-100 object-fit-cover" alt="Gato descansando">
+                    <div class="carousel-caption home-hero-caption">
+                        <h1 class="titulo">Patinhas Peludas</h1>
+                        <p class="texto">Cuidado, acolhimento e novas chances para cada gatinho.</p>
+                    </div>
                 </div>
+
                 <div class="carousel-item">
-                    <img src="https://cdn.prod.website-files.com/635fbf3a618e0a07a4c80313/67eef4ee1730ac44b8d96bcb_gato-solto-no-condominio-o-que-fazer.jpg" 
-                    class="d-block w-100 img-fluid h-100 object-fit-cover" alt="DESCRIÇÃO TEMPORÁRIA, ALTERAR DEPOIS">
+                    <img src="https://cdn.prod.website-files.com/635fbf3a618e0a07a4c80313/67eef4ee1730ac44b8d96bcb_gato-solto-no-condominio-o-que-fazer.jpg" class="d-block w-100 img-fluid h-100 object-fit-cover" alt="Gato olhando para fora">
+                    <div class="carousel-caption home-hero-caption">
+                        <h1 class="titulo"></h1>
+                        <p class="texto"></p>
+                    </div>
                 </div>
+
                 <div class="carousel-item">
-                    <img src="https://images.ecycle.com.br/wp-content/uploads/2023/10/09170201/nathalie-jolie-nKzeG3iE_Qg-unsplash-scaled.jpg.webp" 
-                    class="d-block w-100 img-fluid h-100 object-fit-cover" alt="DESCRIÇÃO TEMPORÁRIA, ALTERAR DEPOIS">
+                    <img src="https://images.ecycle.com.br/wp-content/uploads/2023/10/09170201/nathalie-jolie-nKzeG3iE_Qg-unsplash-scaled.jpg.webp" class="d-block w-100 img-fluid h-100 object-fit-cover" alt="Gato tranquilo">
+                    <div class="carousel-caption home-hero-caption">
+                        <h1 class="titulo"></h1>
+                        <p class="texto"></p>
+                    </div>
                 </div>
             </div>
+
             <button class="carousel-control-prev" type="button" data-bs-target="#indicadoresCarrossel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Anterior</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#indicadoresCarrossel" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Próximo</span>
             </button>
         </div>
-
     </header>
 
-    <div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col">
-      <h1 id='sobre' class="mt-5 ancoras">Um pouco sobre nós:</h1>
-      <p class="texto-sobre">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam iusto fuga tenetur asperiores reiciendis magni? Dicta dolores a hic temporibus commodi expedita aperiam nisi. Repudiandae necessitatibus similique iusto fugiat dolorem?</p>  
-    </div>
-    <div class="col mt-5">
-      <img src="./img/gatoguerra.jpg" alt="gato-de-guerra">
-    </div>  
-  </div>
-</div>
+    <section class="container home-section">
+        <div class="row align-items-center g-4">
+            <div class="col-12 col-md-6">
+                <span class="section-label texto">Nossa hist&oacute;ria</span>
+                <h1 id="sobre" class="titulo ancoras">Um pouco sobre n&oacute;s:</h1>
+                <p class="texto texto-sobre">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam iusto fuga tenetur asperiores reiciendis magni? Dicta dolores a hic temporibus commodi expedita aperiam nisi. Repudiandae necessitatibus similique iusto fugiat dolorem?</p>
+            </div>
+            <div class="col-12 col-md-6">
+                <img class="img-fluid home-about-img" src="./img/gatoguerra.jpg" alt="Gato da ONG">
+            </div>
+        </div>
+    </section>
 
-
-
-
-<!-- Seção de introdução e fotos feita por: Nan Santos -->
-    <section class="container mt-4">
+    <section class="container home-section text-center">
         <div class="d-flex flex-column align-items-center">
             <h1 class="titulo">Patinhas Peludas</h1>
             <h2 class="texto">Gatos amados e bem cuidados!</h2>
@@ -73,37 +77,32 @@ include "inc-head.php";
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-6">
-                    <!-- tamanho 412x453 -->
-                    <img class="object-fit-cover rounded img-galeria" src="./img/calangoo.jpg" width="412" height="453">
+            <div class="row g-3">
+                <div class="col-12 col-md-6">
+                    <img class="object-fit-cover rounded img-galeria" src="./img/calangoo.jpg" width="412" height="453" alt="Gato da galeria">
                 </div>
-                <div class="col-6">
-                    <!-- tamanho 412x453 -->
-                    <img class="object-fit-cover rounded img-galeria" src="./img/irma-xena.jpg" width="412" height="453">
+                <div class="col-12 col-md-6">
+                    <img class="object-fit-cover rounded img-galeria" src="./img/irma-xena.jpg" width="412" height="453" alt="Gato da galeria">
                 </div>
             </div>
 
-            <a href="./galeria.php" class="btn btn-primary fs-4 mt-2 texto">Galeria de Fotos</a>
-
+            <a href="./galeria.php" class="btn btn-patinhas fs-5 mt-3 texto">Nossos Gatinhos</a>
         </main>
     </section>
 
-<!-- Seção de reviews feita por: Nan Santos -->
-    <section class="container mt-4">
+    <section class="container home-section pb-5">
         <div class="row">
             <div class="col-12">
-                <h3 class="titulo">Análises:</h3>
+                <h3 class="titulo text-center">An&aacute;lises:</h3>
             </div>
         </div>
 
-        <div class="row">
-
-            <div class="col-4">
-                <div class="card">
-                    <div class="row">
+        <div class="row g-3">
+            <div class="col-12 col-md-4">
+                <div class="card review-card h-100">
+                    <div class="row g-3 align-items-center">
                         <div class="col-4">
-                            <img src="./img/cat-dant.jpg" class="img-fluid rounded" alt="...">
+                            <img src="./img/cat-dant.jpg" class="img-fluid rounded review-img" alt="Gato em review">
                         </div>
                         <div class="col-8">
                             <h4>Card de review!</h4>
@@ -113,11 +112,11 @@ include "inc-head.php";
                 </div>
             </div>
 
-            <div class="col-4">
-                <div class="card">
-                    <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="card review-card h-100">
+                    <div class="row g-3 align-items-center">
                         <div class="col-4">
-                            <img src="./img/cat-peso.jpg" class="img-fluid rounded" alt="...">
+                            <img src="./img/cat-peso.jpg" class="img-fluid rounded review-img" alt="Gato em review">
                         </div>
                         <div class="col-8">
                             <h4>Card de review!</h4>
@@ -127,11 +126,11 @@ include "inc-head.php";
                 </div>
             </div>
 
-            <div class="col-4">
-                <div class="card">
-                    <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="card review-card h-100">
+                    <div class="row g-3 align-items-center">
                         <div class="col-4">
-                            <img src="./img/sat-cat.jpg" class="img-fluid rounded" alt="...">
+                            <img src="./img/sat-cat.jpg" class="img-fluid rounded review-img" alt="Gato em review">
                         </div>
                         <div class="col-8">
                             <h4>Card de review!</h4>
@@ -140,7 +139,6 @@ include "inc-head.php";
                     </div>
                 </div>
             </div>
-
         </div>
     </section>
 
