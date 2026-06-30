e12582f2debeeab9bb2ed70a752788a70b8ec4b3
Nan Santos <pixelado405@gmail.com>
2026-06-30 20:31:36 -0300

    Update admin-autorizado.php

--- admin-autorizado.php
+++ admin-autorizado.php
@@ -7,8 +7,12 @@ include "inc-head.php";
 ?>
 
 <body class="site-body pt-5 d-flex flex-column min-vh-100">
-    <header>
+    <header class="mb-5">
         <?php include "inc-nav-admin.php" ?>
     </header>
+
+    <section class="mt-4 container page-section d-flex justify-content-center align-items-center">
+        <h1>Patinhas Peludas: Administração</h1>
+    </section>
 
 <?php include "inc-footer-admin.php"; ?>
